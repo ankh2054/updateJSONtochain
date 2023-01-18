b783c388df3be8fe283d4a2564fc98ff15c3fea1
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2023-01-18 09:24:21 +0000

    simplified

--- updateJSON.sh
+++ updateJSON.sh
@@ -8,20 +8,17 @@ EXEC="/home/charles/wax-backup/cleos.sh"
 
 
 createJSON(){
-    #wget --quiet -O - $JSONLOC | jq -M -c '.' >bp.json
-    #sed -i 's/"/\\"/g' bp.json
-    curl -s $JSONLOC | jq -M -c '.' | tr -d '\n' | awk '{gsub(/"/,"\\\""); print}' > bp.json
-}
+     curl $JSONLOC -o bp.json
+ }
 
-updateJSON(){
-    $EXEC  wallet unlock --password $WALLET_PW > /dev/null 2>&1
-    $EXEC push action producerjson set '{"owner":"'$PRODUCER'", "json": "'`printf %q $(cat bp.json | tr -d "\r")`'"}' -p $PRODUCER@active
-    $EXEC wallet lock > /dev/null 2>&1
-    
-}
+ updateJSON(){
+     $EXEC  wallet unlock --password $WALLET_PW > /dev/null 2>&1
+     $EXEC push action producerjson set '{"owner":"'$PRODUCER'", "json": "'`printf %q $(cat bp.json | tr -d "\r")`'"}' -p $PRODUCER@active
+     $EXEC wallet lock > /dev/null 2>&1
 
-createJSON
-updateJSON
+ }
 
+ createJSON
+ updateJSON
 
 
