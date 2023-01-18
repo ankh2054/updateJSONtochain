5413bd998cd1cb25eb3bf22c453c1ec92fdfb107
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2023-01-18 15:23:22 +0000

    ensure spaces in variables are kept

--- updateJSON.sh
+++ updateJSON.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
+# Erase shell variables after use
+set -e
+
 ## Config
 JSONLOC="https://www.sentnl.io/wax.json"
 WALLET_PW=XXXXXXXXXXXXXXXXXXXXXXX
-PRODUCER=sentnlagents
 EXEC="/home/charles/wax-backup/cleos.sh" 
-
+BP_FILE="/tmp/bp.json"
 
 createJSON(){
-     curl $JSONLOC -o bp.json
- }
+curl -s "$JSONLOC" | jq -M -c '.' > $BP_FILE
+PRODUCER=$(cat $BP_FILE | jq -r '.producer_account_name')
+# Escaping all quotes
+perl -pi -e 's/"/\\"/g' $BP_FILE
+}
 
  updateJSON(){
      $EXEC  wallet unlock --password $WALLET_PW > /dev/null 2>&1
-     $EXEC push action producerjson set '{"owner":"'$PRODUCER'", "json": "'`printf %q $(cat bp.json | tr -d "\r")`'"}' -p $PRODUCER@active
+     $EXEC push action producerjson set '{"owner":"'$PRODUCER'","json": "'`cat $BP_FILE`'"}' -p $PRODUCER@active
      $EXEC wallet lock > /dev/null 2>&1
 
  }
-
- createJSON
- updateJSON
-
+createJSON
+updateJSON
 
